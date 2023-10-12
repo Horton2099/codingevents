43876e263fd4a1f544632f82061a745353c8e20a
Austin <kenjigw@gmail.com>
2023-10-11 20:58:19 -0500

    controlindextemplate

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,21 +3,22 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    @GetMapping()
-    public String eventController(Model model){
-        ArrayList<String> eventList = new ArrayList<>();
-        eventList.add("Bog Fest");
-        eventList.add("Annual Maize Celebration");
-        eventList.add("Bob's Music Blast Off");
-        model.addAttribute("events", eventList);
+    private static List<String> events = new ArrayList<>();
+    @GetMapping
+    public String displayAllEvents(Model model){
+        model.addAttribute("events", events);
         return "events/index";
     }
     //lives at /events/create
@@ -25,4 +26,12 @@ public class EventController {
     public String renderCreateEventForm() {
         return "events/create";
     }
+
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
 }
+}
+
